c2821996a5a4b51044e7bff0bc8b11e3bafed3ed
mrkhoa <bluecom123>
2015-11-04 10:53:32 +0700

    Add Brand helper.

---
+++ Brand Extension/Part 3/app/code/local/Boolfly/Brand/Helper/Brand.php
@@ -0,0 +1,23 @@
+<?php
+class Boolfly_Brand_Helper_Brand extends Mage_Core_Helper_Abstract
+{
+
+    /**
+     * Generate brand ulr
+     */
+    public function getBrandUrl(Boolfly_Brand_Model_Brand $brand)
+    {
+        if(! $brand instanceof Boolfly_Brand_Model_Brand) {
+            return '#';
+        }
+        $brandUrl = $brand->getUrlKey();
+        $url = $this->_getUrl('brand/brand/view/' . $brandUrl);
+        return $url;
+    }
+
+    public function isEnabled()
+    {
+        $config = Mage::getStoreConfig('boolfly_brand/enabled');
+        return $config;
+    }
+}

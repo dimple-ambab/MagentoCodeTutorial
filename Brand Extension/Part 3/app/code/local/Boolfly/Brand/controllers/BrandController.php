66c295e7a7b5dd1024c71c8c6ee8a5199903767e
mrkhoa <bluecom123>
2015-11-04 10:51:12 +0700

    Updated Brand controller.

--- Brand Extension/Part 3/app/code/local/Boolfly/Brand/controllers/BrandController.php
+++ Brand Extension/Part 3/app/code/local/Boolfly/Brand/controllers/BrandController.php
@@ -3,28 +3,47 @@ class Boolfly_Brand_BrandController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        echo "Hello World!";
+        $this->loadLayout()->renderLayout();
     }
 
+    public function viewAction()
+    {
+        $brand = Mage::getModel('boolfly_brand/brand');
+        $urlKey = $this->getRequest()->getParam('url_key', '');
+
+        if(strlen($urlKey) > 0) {
+            $brand->load($urlKey, 'url_key');
+        } else {
+            $id = (int)$this->getRequest()->getParam('id', 0);
+            $brand->load($id);
+        }
+
+        if($brand->getId() < 1) {
+            $this->_redirect('*/*/index');
+        }
+
+        Mage::register('current_brand', $brand);
+        $this->loadLayout()->renderLayout();
+    }
     /**
      * Adding brand
      */
-    public function addAction() {
-
-        $brand = Mage::getModel('boolfly_brand/brand');
-        $brand->setData('name', 'Nike');
-        $brand->setData('description', 'Nike');
-        $brand->setData('visibility', 1);
-        $brand->setData('url_key', 'nike');
-        try {
-            $brand->save();
-            echo "Successfully saved brand";
-
-        } catch (Exception $e) {
-            echo $e->getMessage();
-            Mage::logException($e);
-        }
-    }
+//    public function addAction() {
+//
+//        $brand = Mage::getModel('boolfly_brand/brand');
+//        $brand->setData('name', 'Nike');
+//        $brand->setData('description', 'Nike');
+//        $brand->setData('visibility', 1);
+//        $brand->setData('url_key', 'nike');
+//        try {
+//            $brand->save();
+//            echo "Successfully saved brand";
+//
+//        } catch (Exception $e) {
+//            echo $e->getMessage();
+//            Mage::logException($e);
+//        }
+//    }
 
     /**
      * Show brands
